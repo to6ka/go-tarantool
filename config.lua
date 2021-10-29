fd68e125e7f13b4150e7e3b8dc6586bf91fceca1
Georgy Moiseev <georgy.moiseev@corp.mail.ru>
2021-10-29 17:34:06 +0300

    Support UUID type in msgpack
    This patch provides UUID support for all space operations and as
    function return result. UUID type was introduced in Tarantool 2.4.1.
    See more in commit messages [1].
    
    To use UUID with google/uuid in msgpack, import
    tarantool/go-tarantool/uuid submodule.
    
    1. https://github.com/tarantool/tarantool/commit/d68fc29246714eee505bc9bbcd84a02de17972c5
    
    Closes #90

--- config.lua
+++ config.lua
@@ -61,3 +61,24 @@ console.listen '0.0.0.0:33015'
 
 --box.schema.user.revoke('guest', 'read,write,execute', 'universe')
 
+-- Create space with UUID pk if supported
+local uuid = require('uuid')
+local msgpack = require('msgpack')
+
+local uuid_msgpack_supported = pcall(msgpack.encode, uuid.new())
+if uuid_msgpack_supported then
+    local suuid = box.schema.space.create('testUUID', {
+        id = 524,
+        if_not_exists = true,
+    })
+    suuid:create_index('primary', {
+        type = 'tree',
+        parts = {{ field = 1, type = 'uuid' }},
+        if_not_exists = true
+    })
+    suuid:truncate()
+
+    box.schema.user.grant('test', 'read,write', 'space', 'testUUID', { if_not_exists = true })
+
+    suuid:insert({ uuid.fromstr("c8f0fa1f-da29-438c-a040-393f1126ad39") })
+end
